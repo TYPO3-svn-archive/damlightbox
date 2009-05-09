72f0f023a66891d3bdad7db198c7f46077bd8715
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-09 07:07:35 +0000

    Refactoring of files and folder structure
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/damlightbox/trunk@20104 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/scripts/damlightbox.js
+++
@@ -1,2 +0,0 @@
-var compatible = (document.getElementsByTagName && document.createElement);
-if (compatible) document.write('<style type="text/css"> #lbCanvas #lbNextLink {right: 0 !important;} #imagebrowser,#description {display:none;}</style>');
