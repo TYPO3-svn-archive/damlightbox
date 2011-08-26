0f8fdfd17a61594a3084c2a5749c1b1e87e82faa
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-26 08:38:28 +0000

    Version 0.1.2 released to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/damlightbox/trunk@51418 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "damlightbox".
 #
-# Auto generated 29-07-2010 23:26
+# Auto generated 26-08-2011 10:36
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Show your DAM images with the metadata of your choice in the frontend and within a templateable popup/lightbox',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'dependencies' => 'dam',
 	'conflicts' => '',
 	'priority' => '',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'dam_pages' => '0.1.7-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:43:{s:13:"CHANGELOG.txt";s:4:"1f6e";s:8:"TODO.txt";s:4:"28dc";s:20:"class.ext_update.php";s:4:"a5a9";s:21:"ext_conf_template.txt";s:4:"e5b2";s:12:"ext_icon.gif";s:4:"ffed";s:17:"ext_localconf.php";s:4:"3eb6";s:14:"ext_tables.php";s:4:"ec16";s:14:"ext_tables.sql";s:4:"4c40";s:15:"flexform_ds.xml";s:4:"1f26";s:16:"locallang_db.xml";s:4:"fffd";s:22:"locallang_flexform.xml";s:4:"0ad5";s:14:"doc/manual.sxw";s:4:"22d0";s:37:"hooks/class.tx_damlightbox_dblist.php";s:4:"3245";s:38:"hooks/class.tx_damlightbox_tceform.php";s:4:"888b";s:38:"hooks/class.tx_damlightbox_tcemain.php";s:4:"65df";s:13:"mod1/conf.php";s:4:"d594";s:14:"mod1/index.php";s:4:"baea";s:18:"mod1/locallang.xml";s:4:"7c84";s:22:"mod1/locallang_mod.xml";s:4:"c7e4";s:22:"mod1/mod_template.html";s:4:"3314";s:19:"mod1/moduleicon.gif";s:4:"ffed";s:32:"pi1/class.tx_damlightbox_div.php";s:4:"6529";s:34:"pi1/class.tx_damlightbox_pages.php";s:4:"f587";s:32:"pi1/class.tx_damlightbox_pi1.php";s:4:"1f83";s:39:"pi1/class.tx_damlightbox_pmkslimbox.php";s:4:"01ca";s:36:"pi1/class.tx_damlightbox_realurl.php";s:4:"4bb1";s:38:"pi1/class.tx_damlightbox_ttaddress.php";s:4:"4242";s:35:"pi1/class.tx_damlightbox_ttnews.php";s:4:"dda1";s:25:"res/basic/damlightbox.css";s:4:"6c25";s:23:"res/basic/template.html";s:4:"10c7";s:30:"res/pmkslimbox/damlightbox.css";s:4:"ff1e";s:29:"res/pmkslimbox/slimboxplus.js";s:4:"1493";s:42:"res/pmkslimbox/slimboxplus_uncompressed.js";s:4:"6051";s:28:"res/pmkslimbox/template.html";s:4:"96af";s:20:"static/constants.txt";s:4:"120d";s:16:"static/setup.txt";s:4:"d2fc";s:22:"static/pages/setup.txt";s:4:"354b";s:31:"static/pmkslimbox/constants.txt";s:4:"a65d";s:27:"static/pmkslimbox/setup.txt";s:4:"84b6";s:27:"static/tt_address/setup.txt";s:4:"c617";s:27:"static/tt_content/setup.txt";s:4:"9e7e";s:24:"static/tt_news/setup.txt";s:4:"9db5";s:27:"static/watermarks/setup.txt";s:4:"f144";}',
+	'_md5_values_when_last_written' => 'a:43:{s:13:"CHANGELOG.txt";s:4:"d20b";s:8:"TODO.txt";s:4:"28dc";s:20:"class.ext_update.php";s:4:"a5a9";s:21:"ext_conf_template.txt";s:4:"e5b2";s:12:"ext_icon.gif";s:4:"ffed";s:17:"ext_localconf.php";s:4:"b889";s:14:"ext_tables.php";s:4:"ec16";s:14:"ext_tables.sql";s:4:"4c40";s:15:"flexform_ds.xml";s:4:"1f26";s:16:"locallang_db.xml";s:4:"fffd";s:22:"locallang_flexform.xml";s:4:"0ad5";s:14:"doc/manual.sxw";s:4:"6172";s:37:"hooks/class.tx_damlightbox_dblist.php";s:4:"3245";s:38:"hooks/class.tx_damlightbox_tceform.php";s:4:"4a8c";s:38:"hooks/class.tx_damlightbox_tcemain.php";s:4:"65df";s:13:"mod1/conf.php";s:4:"d594";s:14:"mod1/index.php";s:4:"baea";s:18:"mod1/locallang.xml";s:4:"7c84";s:22:"mod1/locallang_mod.xml";s:4:"c7e4";s:22:"mod1/mod_template.html";s:4:"3314";s:19:"mod1/moduleicon.gif";s:4:"ffed";s:32:"pi1/class.tx_damlightbox_div.php";s:4:"6529";s:34:"pi1/class.tx_damlightbox_pages.php";s:4:"f587";s:32:"pi1/class.tx_damlightbox_pi1.php";s:4:"e686";s:39:"pi1/class.tx_damlightbox_pmkslimbox.php";s:4:"01ca";s:36:"pi1/class.tx_damlightbox_realurl.php";s:4:"4bb1";s:38:"pi1/class.tx_damlightbox_ttaddress.php";s:4:"4242";s:35:"pi1/class.tx_damlightbox_ttnews.php";s:4:"dda1";s:25:"res/basic/damlightbox.css";s:4:"6c25";s:23:"res/basic/template.html";s:4:"10c7";s:30:"res/pmkslimbox/damlightbox.css";s:4:"ff1e";s:29:"res/pmkslimbox/slimboxplus.js";s:4:"1493";s:42:"res/pmkslimbox/slimboxplus_uncompressed.js";s:4:"6051";s:28:"res/pmkslimbox/template.html";s:4:"96af";s:20:"static/constants.txt";s:4:"120d";s:16:"static/setup.txt";s:4:"6aa3";s:22:"static/pages/setup.txt";s:4:"354b";s:31:"static/pmkslimbox/constants.txt";s:4:"a65d";s:27:"static/pmkslimbox/setup.txt";s:4:"84b6";s:27:"static/tt_address/setup.txt";s:4:"c617";s:27:"static/tt_content/setup.txt";s:4:"9e7e";s:24:"static/tt_news/setup.txt";s:4:"9db5";s:27:"static/watermarks/setup.txt";s:4:"f144";}',
 	'suggests' => array(
 	),
 );
